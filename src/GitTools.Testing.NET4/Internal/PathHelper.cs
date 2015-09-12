7e6897de0052899ab280b83548c753c9374a00a1
Jake Ginnivan <jake@ginnivan.net>
2015-09-12 22:31:47 +0800

    Changed the test repositories path to hopefully not hit the MAX_PATH when shadow copy is on for tests

--- src/GitTools.Testing.NET4/Internal/PathHelper.cs
+++ src/GitTools.Testing.NET4/Internal/PathHelper.cs
@@ -2,18 +2,12 @@
 {
     using System;
     using System.IO;
-    using System.Reflection;
 
     static class PathHelper
     {
         public static string GetTempPath()
         {
-            return Path.Combine(GetCurrentDirectory(), "TestRepositories", Guid.NewGuid().ToString());
-        }
-
-        private static string GetCurrentDirectory()
-        {
-            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return Path.Combine(Path.GetTempPath(), "TestRepositories", Guid.NewGuid().ToString());
         }
     }
 }
